a4f770f7396969c899ebaf06920f1f5a262ee990
Andre Cruz <ajcruzg95@gmail.com>
2022-12-17 08:59:08 -0500

    finalizado

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\User;
+class LoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_user_can_register()
+    {
+        
+        $this->assertTrue(true);
+    }
+}
